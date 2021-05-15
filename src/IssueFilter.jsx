f43d2d5c622341930715de120d2954a98b5aa60f
philthefox <43797286+philthefox@users.noreply.github.com>
2021-05-15 20:36:19 +0200

    First commit

---
+++ src/IssueFilter.jsx
@@ -0,0 +1,18 @@
+
+/* eslint "react/prefer-stateless-function": "off" */
+
+import React from 'react';
+
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href="/#/issues">All Issues</a>
+        {' | '}
+        <a href="/#/issues?status=New">New Issues</a>
+        {' | '}
+        <a href="/#/issues?status=Assigned">Assigned Issues</a>
+      </div>
+    );
+  }
+}
