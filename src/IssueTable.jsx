3eee49d43a4425268310c149d980a9f5a3866514
philthefox <43797286+philthefox@users.noreply.github.com>
2021-05-17 13:09:17 +0200

    Small changes

--- src/IssueTable.jsx
+++ src/IssueTable.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function IssueRow({ issue }) { // stateless component
   return (
@@ -11,7 +12,7 @@ function IssueRow({ issue }) { // stateless component
       <td>{issue.effort}</td>
       <td>{issue.due ? issue.due.toDateString() : ''}</td>
       <td>{issue.title}</td>
-      <td><a href={`/#/edit/${issue.id}`}>Edit</a></td>
+      <td><Link to={`/edit/${issue.id}`}>Edit</Link></td>
     </tr>
   );
 }
