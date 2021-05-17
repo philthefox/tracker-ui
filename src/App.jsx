f5fb4646f503bb0d0cf4834045aa5e5bd24f612b
philthefox <43797286+philthefox@users.noreply.github.com>
2021-05-17 14:17:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { HashRouter as Router } from 'react-router-dom';
 import Page from './Page.jsx';
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./src/sw.js', { scope: './src/' })
+  navigator.serviceWorker.register('sw.js')
     .then((reg) => {
       // registration worked
       console.log(`Registration succeeded. Scope is ${reg.scope}`);
